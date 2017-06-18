163d730400842f8f7e7560927579a92fd0ec3a89
Manuel Trules <manu77974@MacBook-Pro-de-Manuel.local>
2017-06-18 16:10:15 +0200

    Ajout composant connexion

---
+++ Vocal.MobileApp/src/pages/connexion/connexion.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-connexion',
+  templateUrl: 'connexion.html'
+})
+export class Connexion {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
