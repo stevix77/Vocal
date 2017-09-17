cdbbdd205c066e516c47a6509a4c0f71c2101f10
Manuel Trules <manu77974@gmail.com>
2017-09-17 20:47:35 +0200

    Edit vocal with filter

--- Vocal.MobileApp/src/services/audiorecorder.ts
+++ Vocal.MobileApp/src/services/audiorecorder.ts
@@ -30,12 +30,21 @@ export class AudioRecorder {
 
   applyEffect(filter) {
     let contexteAudio = this.contexteAudio;
-    this.file.readAsArrayBuffer(this.file.dataDirectory, 'recording.m4a').then((compressedBuffer) => {
+    var that = this;
+    this.file.readAsArrayBuffer(this.file.dataDirectory, this.filename).then((compressedBuffer) => {
       contexteAudio.decodeAudioData(compressedBuffer, function(buffer){
         var source = contexteAudio.createBufferSource();
         source.buffer = buffer;
 
-        var effect = this.getEffectFromFilter(filter);
+        //var effect = that.getEffectFromFilter(filter);
+        var effect = new that.tuna.Phaser({
+            rate: 1.2,                     //0.01 to 8 is a decent range, but higher values are possible
+            depth: 0.3,                    //0 to 1
+            feedback: 0.2,                 //0 to 1+
+            stereoPhase: 30,               //0 to 180
+            baseModulationFrequency: 700,  //500 to 1500
+            bypass: 0
+        });
 
         var output = contexteAudio.createGain();
         source.connect(effect);
@@ -43,7 +52,7 @@ export class AudioRecorder {
 
         output.connect(contexteAudio.destination);
         source.start();
-      }).bind(this);
+      }).catch(err => console.log(err));
     }).catch(err => console.log(err));
   }
 
@@ -72,21 +81,12 @@ export class AudioRecorder {
   }
 
   getFile() : Promise<string>{
-    //return this.file.readAsDataURL(this.file.tempDirectory, this.filename);
-    //return this.file.readAsDataURL('../Library/NoCloud/', 'recording.m4a');
-    return this.file.readAsDataURL(this.file.dataDirectory, 'recording.m4a');
+    return this.file.readAsDataURL(this.file.dataDirectory, this.filename);
   }
 
   getMedia() {
     if(this.mediaObject == null) {
-      //this.file.createFile(this.file.dataDirectory.replace(/^file:\/\//, ''), 'recording.m4a');
-      //this.mediaObject = this.media.create(this.file.tempDirectory.replace(/^file:\/\//, '') + this.filename)
-      // this.file.createFile('../Library/NoCloud/', 'recording.m4a', true).then(() => {
-        this.mediaObject = this.media.create(this.file.dataDirectory.replace(/^file:\/\//, '') + 'recording.m4a');
-        console.log(this.mediaObject);
-      // }, (err) => {
-      //   console.log(err);
-      // });
+      this.mediaObject = this.media.create(this.file.dataDirectory.replace(/^file:\/\//, '') + this.filename);
     }
     return this.mediaObject;
   }
