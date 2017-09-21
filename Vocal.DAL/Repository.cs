bae17549c7a134fe3f9b3e81ab91d167853e5274
svale <svale@STEVIX-PC>
2017-09-21 23:02:14 +0200

    Optimisation

--- Vocal.DAL/Repository.cs
+++ Vocal.DAL/Repository.cs
@@ -264,13 +264,14 @@ namespace Vocal.DAL
                 List<People> people = Bind_UsersToFriends(users);
                 people.RemoveAll(x => user.Friends.Select(y => y.Id).Contains(x.Id));
                 user.Following.AddRange(people);
-                Task.Run(() =>
-                {
-                    foreach (var p in people)
-                    {
-                        AddInFollowers(p.Id, user.Id);
-                    }
-                });
+                //Task.Run(() =>
+                //{
+                //    foreach (var p in people)
+                //    {
+                //        AddInFollowers(p.Id, user.Id);
+                //    }
+                //});
+                Parallel.ForEach(people, (p) => AddInFollowers(p.Id, user.Id));
                 db.ReplaceOne(x => x.Id == userId, user);
                 success = true;
             }
