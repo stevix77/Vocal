112c0f727e5bc97efee654891398c5cc39d62543
Manuel Trules <manu77974@gmail.com>
2017-10-04 08:05:08 +0200

    Search by mail

--- Vocal.MobileApp/src/pages/search-mail/search-mail.ts
+++ Vocal.MobileApp/src/pages/search-mail/search-mail.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { SearchFriendsRequest } from '../../models/request/searchFriendsRequest';
+import { params } from '../../services/params';
+import { url } from '../../services/url';
+import { Response } from '../../models/response';
+import { HttpService } from '../../services/httpService';
+import { CookieService } from '../../services/cookieService';
+import { UserResponse } from '../../models/response/userResponse';
 
 /**
  * Generated class for the SearchMailPage page.
@@ -11,10 +18,38 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-search-mail',
   templateUrl: 'search-mail.html',
+  providers: [HttpService, CookieService]
 })
 export class SearchMailPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams, 
+    private cookieService: CookieService, 
+    private httpService: HttpService) {
+  }
+
+  viewUsersByMail(val) {
+    let emails = [val];
+    this.searchFriends(emails);
+  }
+
+  searchFriends(emails: Array<string>) {
+    let obj = new SearchFriendsRequest();
+    obj.Lang = params.Lang;
+    obj.Emails = emails;
+    let urlSearch = url.SearchFriends();
+    let cookie = this.cookieService.GetAuthorizeCookie(urlSearch, params.User)
+    this.httpService.Post<SearchFriendsRequest>(urlSearch, obj, cookie).subscribe(
+      resp => { 
+        let response = resp.json() as Response<Array<UserResponse>>;
+        console.log(response);
+        // if(!response.HasError) {
+        //   this.model.Friends = response.Data;
+        // } else {
+        //   this.model.ErrorFriends = response.ErrorMessage;
+        // }
+      }
+    );
   }
 
   ionViewDidLoad() {
