2ec88d32ddc34732de9669262d88d51a21a2e4b7
Christopher GABIN <titoun_k@hotmail.fr>
2017-10-08 17:28:49 +0200

    Exemple query get convers

--- Vocal.Model/DataBaseObject/Talk.cs
+++ Vocal.Model/DataBaseObject/Talk.cs
@@ -1,10 +1,13 @@
-﻿
+﻿using System;
+
 namespace Vocal.Model.DataBaseObject
 {
     public class Talk
     {
         public Message Message { get; set; }
+        public DateTime date { get; set; }
         public IRecipient Contact { get; set; }
+        public string IdContact { get; set; }
         public bool IsArchived { get; set; }
         public bool IsDeleted { get; set; }
         public bool IsReceivedMessage { get; set; }
