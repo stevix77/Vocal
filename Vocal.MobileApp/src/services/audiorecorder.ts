d3fc849b3aeec68b982e19d225c41ba8266c9aae
Manuel Trules <manu77974@gmail.com>
2017-10-31 19:49:16 +0100

    Record vocal is back

--- Vocal.MobileApp/src/services/audiorecorder.ts
+++ Vocal.MobileApp/src/services/audiorecorder.ts
@@ -78,19 +78,17 @@ export class AudioRecorder {
     }
   }
 
+  // Used in send-vocal.ts
   getFile() : Promise<string>{
     return this.file.readAsDataURL(this.file.dataDirectory, this.filename);
+    return this.file.readAsDataURL('../Library/NoCloud/', this.filename);
   }
 
   getMedia() {
-    console.log(this.file.dataDirectory);
     if(this.mediaObject == null) {
-      this.mediaObject = this.media.create(this.file.dataDirectory.replace(/^file:\/\//, '') + this.filename);
-      console.log('if null');
-      console.log(this.mediaObject);
+      //this.mediaObject = this.media.create(this.file.dataDirectory + this.filename);
+      this.mediaObject = this.media.create('../Library/NoCloud/' + this.filename);
     } else {
-      console.log('else');
-      console.log(this.mediaObject);
     }
     return this.mediaObject;
   }
