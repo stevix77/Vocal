38830599ef6e823698c801c9db085c35a56592b5
Steve <Steve@HOME-PC>
2017-11-01 02:14:59 +0100

    Revert "Baisser la qualité des photos augmenter taille limit request"
    This reverts commit 2fbdbc8249def27cd4b390acd3d19055a47736ed.

--- Vocal.MobileApp/src/pages/modal-profile/modal-profile.ts
+++ Vocal.MobileApp/src/pages/modal-profile/modal-profile.ts
@@ -30,7 +30,7 @@ export class ModalProfilePage {
   private User: AppUser;
   private CountFriendsAddedMe: number = 0;
   private options: CameraOptions = {
-    quality: 50,
+    quality: 100,
     destinationType: this.camera.DestinationType.DATA_URL,
     encodingType: this.camera.EncodingType.JPEG,
     mediaType: this.camera.MediaType.PICTURE,
