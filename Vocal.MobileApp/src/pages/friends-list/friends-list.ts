566bc7ffdaf657de9cb089e44b7a0ab8f5809df1
Manuel Trules <manu77974@gmail.com>
2017-11-29 11:31:37 +0100

    Block contact

--- Vocal.MobileApp/src/pages/friends-list/friends-list.ts
+++ Vocal.MobileApp/src/pages/friends-list/friends-list.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, ActionSheetController } from 'ionic-angular';
 import { UserResponse } from "../../models/response/userResponse";
 import { KeyStore } from "../../models/enums";
 import { StoreService } from "../../services/storeService";
@@ -12,6 +12,7 @@ import { Response } from '../../models/response';
 import { ManageFriendsRequest } from "../../models/request/manageFriendsRequest";
 import { PeopleResponse } from "../../models/response/peopleResponse";
 import { MessagePage } from "../message/message";
+import { forkJoin } from "rxjs/observable/forkJoin";
 
 /**
  * Generated class for the FriendsListPage page.
@@ -29,6 +30,7 @@ export class FriendsListPage {
   constructor(public navCtrl: NavController, 
     public alertCtrl: AlertController,
     public navParams: NavParams,
+    public actionSheetCtrl: ActionSheetController,
     private storeService: StoreService, 
     private httpService: HttpService, 
     private cookieService: CookieService) {
@@ -95,7 +97,30 @@ export class FriendsListPage {
     this.navCtrl.push(MessagePage, {TalkId: null, UserId: userId})
   }
 
-  showConfirm(userId) {
+  showBlockContact(_id:string) {
+    let id = _id;
+    let confirm = this.alertCtrl.create({
+      title: 'Bloquer un contact',
+      message: 'Êtes-vous sûr de vouloir bloquer ce contact ?',
+      buttons: [
+        {
+          text: 'Non',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: 'Oui',
+          handler: () => {
+            this.blockContact(id);;
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
+
+  showDeleteContact(userId) {
     let confirm = this.alertCtrl.create({
       title: 'Supprimer un contact',
       message: 'Êtes-vous sûr de vouloir supprimer ce contact ?',
@@ -115,6 +140,81 @@ export class FriendsListPage {
       ]
     });
     confirm.present();
+  }
+
+  blockContact(userId:string) {
+    let obj = new ManageFriendsRequest();
+    obj.Lang = params.Lang;
+    obj.UserId = params.User.Id;
+    obj.Ids = [userId];
+    let urlFriends = url.BlockPeople();
+    let urlDelete = url.RemoveFriends();
+    let cookie = this.cookieService.GetAuthorizeCookie(urlFriends, params.User);
+    let cookieDelete = this.cookieService.GetAuthorizeCookie(urlDelete, params.User);
+    
+    forkJoin(this.httpService.Post<ManageFriendsRequest>(urlFriends, obj, cookie), this.httpService.Post<ManageFriendsRequest>(urlDelete, obj, cookieDelete)).subscribe(
+      responses => {
+        let response = responses[0].json() as Response<boolean>;
+        if(!response.HasError) {
+          let index = this.Friends.findIndex(x => x.Id == userId);
+          this.Friends.splice(index, 1);
+          this.storeService.Set(KeyStore[KeyStore.Friends], this.Friends)
+        } else {
+          console.log("error ->" + response.ErrorMessage);
+        }
+      }
+    );
+
+    // this.httpService.Post<ManageFriendsRequest>(urlFriends, obj, cookie).subscribe(
+    //   resp => { 
+    //     let response = resp.json() as Response<boolean>;
+    //     if(!response.HasError) {
+    //       let index = this.Friends.findIndex(x => x.Id == userId);
+    //       this.Friends.splice(index, 1);
+    //       this.storeService.Set(KeyStore[KeyStore.Friends], this.Friends)
+    //     } else {
+    //       console.log("error ->" + response.ErrorMessage);
+    //     }
+    //   }
+    // );
+  }
+
+  presentActionSheet(_id:string){
+    let id = _id;
+    let actionSheet = this.actionSheetCtrl.create({
+      buttons: [
+        {
+          text: 'Message',
+          handler: () => {
+            this.sendMessage(id);
+          }
+        },
+        {
+          text: 'Bloquer',
+          role: 'destructive',
+          handler: () => {
+            this.showBlockContact(id);
+            console.log('Bloquer clicked');
+          }
+        },
+        {
+          text: 'Supprimer',
+          role: 'destructive',
+          handler: () => {
+            this.showDeleteContact(id);
+          }
+        },
+        {
+          text: 'Annuler',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+ 
+    actionSheet.present();
   }
 
 }
