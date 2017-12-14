148ef90231b82673758bc3567536c5ffc44d4dd9
svale <svale@STEVIX-PC>
2017-12-14 23:27:52 +0100

    backupbusiness

---
+++ Vocal.Business/Backup/BackupBusiness.cs
@@ -0,0 +1,45 @@
+﻿using MongoDB.Bson;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Vocal.DAL;
+
+namespace Vocal.Business.Backup
+{
+    public class BackupBusiness
+    {
+        public static void ExportCollections()
+        {
+            try
+            {
+                while(true)
+                {
+                    var collections = Repository.Instance.GetAllCollections();
+                    foreach (var item in collections)
+                    {
+                        var name = item.GetElement("name").Value.ToString();
+                        var docs = Repository.Instance.GetDocuments(name);
+                        if (docs.Count > 0)
+                        {
+                            var json = JsonConvert.SerializeObject(docs);
+                            if (!Directory.Exists("export"))
+                                Directory.CreateDirectory("export");
+                            File.WriteAllText($"export/{name}.json", json);
+                        }
+                    }
+                    Thread.Sleep(1000 * 3600 * 6);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
