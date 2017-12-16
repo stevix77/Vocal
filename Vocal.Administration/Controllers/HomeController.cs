ccf7f6d31da3ea413b751eceff1c8ecabc69d343
krisGB97 <christopher.gabin@gmail.com>
2017-12-16 14:25:49 +0100

    back back

---
+++ Vocal.Administration/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vocal.Administration.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
