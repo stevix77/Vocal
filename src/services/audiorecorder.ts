49ada04e3fe66c162020e2830d1358c6fcabaf2a
Manuel Trules <manu77974@gmail.com>
2018-03-21 03:37:14 +0100

    Jouer un son avec Web Audio API

--- src/services/audiorecorder.ts
+++ src/services/audiorecorder.ts
@@ -4,6 +4,7 @@ import { Timer } from './timer';
 import { Media, MediaObject } from '@ionic-native/media';
 import { File } from '@ionic-native/file';
 import { ExceptionService } from './exceptionService';
+import { typeSourceSpan } from '@angular/compiler';
 
 @Injectable()
 export class AudioRecorder {
@@ -13,7 +14,8 @@ export class AudioRecorder {
   isApp: boolean;
   effects: Array<Object> = new Array();
   tuna: any;
-  contexteAudio: any;
+  context: any;
+  isPlaying: boolean = false;
   constructor(
     public config:Config,
     public alertCtrl: AlertController,
@@ -23,36 +25,66 @@ export class AudioRecorder {
     private exceptionService: ExceptionService) {
     this.filename = 'recording.' + this.getExtension();
     this.isApp = this.config.get('isApp');
-    //this.initEffects();
   }
 
   applyEffect(filter) {
-    let contexteAudio = this.contexteAudio;
-    var that = this;
-    //this.file.readAsArrayBuffer(this.file.dataDirectory, this.filename).then((compressedBuffer) => {
-    this.file.readAsArrayBuffer(this.getFilePath(), this.filename).then((compressedBuffer) => {
-      contexteAudio.decodeAudioData(compressedBuffer, function(buffer){
-        var source = contexteAudio.createBufferSource();
-        source.buffer = buffer;
-
-        //var effect = that.getEffectFromFilter(filter);
-        var effect = new that.tuna.Phaser({
-            rate: 1.2,                     //0.01 to 8 is a decent range, but higher values are possible
-            depth: 0.3,                    //0 to 1
-            feedback: 0.2,                 //0 to 1+
-            stereoPhase: 30,               //0 to 180
-            baseModulationFrequency: 700,  //500 to 1500
-            bypass: 0
+    console.log('apply effect')
+    if (!this.isPlaying) {
+      // Make a source node for the sample.
+      let source = this.context.createBufferSource();
+      let context = this.context;
+      this.file.readAsArrayBuffer(this.getFilePath(), this.filename).then(arrayBuffer => {
+        context.decodeAudioData(arrayBuffer, function(buffer){
+          source.buffer = buffer;
+          source.connect(context.destination);
+          source.start();
+          // Si le son est coupé sur l'iPhone, l'audio ne se joue pas
         });
+      }).catch(err => console.log('error readAsArrayBuffer : ' + err));
+      //source.buffer = this.buffer;
+      // // Make a convolver node for the impulse response.
+      // var convolver = context.createConvolver();
+      // convolver.buffer = this.impulseResponseBuffer;
+      // // Connect the graph.
+      // source.connect(convolver);
+      // convolver.connect(context.destination);
+      // // Save references to important nodes.
+      // this.source = source;
+      // this.convolver = convolver;
+      // // Start playback.
+      // this.source[this.source.start ? 'start': 'noteOn'](0);
+    } else {
+      console.log('else');
+      //this.source[this.source.stop ? 'stop': 'noteOff'](0);
+    }
+    //this.isPlaying = !this.isPlaying;
 
-        var output = contexteAudio.createGain();
-        source.connect(effect);
-        effect.connect(output);
+    // let contexteAudio = this.contexteAudio;
+    // var that = this;
+    // //this.file.readAsArrayBuffer(this.file.dataDirectory, this.filename).then((compressedBuffer) => {
+    // this.file.readAsArrayBuffer(this.getFilePath(), this.filename).then((compressedBuffer) => {
+    //   contexteAudio.decodeAudioData(compressedBuffer, function(buffer){
+    //     var source = contexteAudio.createBufferSource();
+    //     source.buffer = buffer;
 
-        output.connect(contexteAudio.destination);
-        source.start();
-      }).catch(err => console.log(err));
-    }).catch(err => console.log(err));
+    //     //var effect = that.getEffectFromFilter(filter);
+    //     var effect = new that.tuna.Phaser({
+    //         rate: 1.2,                     //0.01 to 8 is a decent range, but higher values are possible
+    //         depth: 0.3,                    //0 to 1
+    //         feedback: 0.2,                 //0 to 1+
+    //         stereoPhase: 30,               //0 to 180
+    //         baseModulationFrequency: 700,  //500 to 1500
+    //         bypass: 0
+    //     });
+
+    //     var output = contexteAudio.createGain();
+    //     source.connect(effect);
+    //     effect.connect(output);
+
+    //     output.connect(contexteAudio.destination);
+    //     source.start();
+    //   }).catch(err => console.log(err));
+    // }).catch(err => console.log(err));
   }
 
   editPlayback(filter) {
@@ -85,11 +117,10 @@ export class AudioRecorder {
 
   // Used in send-vocal.ts
   getFile() : Promise<string>{
-    console.log(this.getFilePath());
-    console.log(this.filename);
     return this.file.readAsDataURL(this.getFilePath(), this.filename);
   }
 
+  /*
   initEffects(){
     this.contexteAudio = new (window["AudioContext"] || window["webkitAudioContext"])();
     this.tuna = new window["Tuna"](this.contexteAudio);
@@ -120,6 +151,7 @@ export class AudioRecorder {
         bypass: 0
     });
   }
+  */
 
   release() {
     this.mediaObject.release();
@@ -130,10 +162,18 @@ export class AudioRecorder {
     this.file.createFile(this.getFilePath(), this.filename, true).then(() => {
       let path = this.getFilePath().replace(/^file:\/\//, '');
       this.mediaObject = this.media.create(path + this.filename);
+      this.mediaObject.onStatusUpdate.subscribe(this.onMediaStatusUpdate);
+      this.context = new (window["AudioContext"] || window["webkitAudioContext"])();
       this.mediaObject.startRecord();
     }).catch(err => {
         this.exceptionService.Add(err);
     });
+  }
+
+  onMediaStatusUpdate(status) {
+    if(status == 2) this.isPlaying = true;
+    if(status == 4) this.isPlaying = false;
+    console.log('isPlaying : ' + this.isPlaying);
   }
 
   startRecording() {
@@ -148,11 +188,15 @@ export class AudioRecorder {
   }
 
   startPlayback() {
-    if(this.isApp) this.mediaObject.play();
+    if(this.isApp) {
+      this.mediaObject.play();
+    }
   }
 
   stopPlayback() {
-    if(this.isApp) this.mediaObject.stop();
+    if(this.isApp) {
+      this.mediaObject.stop();
+    }
   }
 
   showAlert(message) {
