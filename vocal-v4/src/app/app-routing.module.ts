ae2f77cc4bc484f419613b187f23c7c2bf4a0889
Manuel Trules <manuel.trules@gmail.com>
2020-04-21 23:58:53 +0200

    Ajout de la page feed / Ajout de l'auth

--- vocal-v4/src/app/app-routing.module.ts
+++ vocal-v4/src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'feed', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   {
     path: 'connexion',
     loadChildren: () => import('./pages/connexion/connexion.module').then( m => m.ConnexionPageModule)
+  },
+  {
+    path: 'feed',
+    canActivate: [AuthGuard],
+    loadChildren: () => import('./pages/feed/feed.module').then( m => m.FeedPageModule)
   }
 ];
 
