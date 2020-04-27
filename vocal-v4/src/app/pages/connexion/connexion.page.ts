2afc3673095fa0243a4577a6ce53de65c19db1b6
Manuel Trules <manuel.trules@gmail.com>
2020-04-28 01:04:28 +0200

    delete unused imports

--- vocal-v4/src/app/pages/connexion/connexion.page.ts
+++ vocal-v4/src/app/pages/connexion/connexion.page.ts
@@ -1,14 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { InitService } from 'src/app/services/init.service';
-import { ToastController } from '@ionic/angular';
-import { ExceptionService } from 'src/app/services/exception.service';
-import { CryptService } from 'src/app/services/crypt.service';
-import { params } from 'src/app/services/params';
-import { Response } from 'src/app/models/response';
-import { UserResponse } from 'src/app/models/response/userResponse';
-import { InitResponse } from 'src/app/models/response/InitResponse';
-import { Route } from '@angular/compiler/src/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-connexion',
